f61be0e9e1fed2bcacebdc031152ebc14442f5c0
EinarLab <einharilab@gmail.com>
2025-07-12 14:43:24 +0300

    update content

--- src/components/Design.svelte
+++ src/components/Design.svelte
@@ -1,17 +1,23 @@
 <script>
     import DesignItem from "./DesignItem.svelte";
 
-    import bgs from "$lib/img/projects-background/1.jpg";
+    import bgsAppartment from "$lib/img/design-background/Дизайн-проект-квартиры.png";
+    import bgsAppartmentJKPritajenia from "$lib/img/design-background/Дизайн-проект-квартиры-ЖК-Притяжение.png";
+    import bgsAppartmentJKSarmanovskiy from "$lib/img/design-background/Дизайн-проект-квартиры-ЖК-Сармановский.png";
+    import bgsReconstructionPrivateHouseGabishego from "$lib/img/design-background/Реконструкция-частного-дома-Габишего.png";
 
     const designBgs = [
-        bgs
-    ]
+        bgsAppartment,
+        bgsAppartmentJKPritajenia,
+        bgsAppartmentJKSarmanovskiy,
+        bgsReconstructionPrivateHouseGabishego,
+    ];
 
     const designTexts = [
-        'Дизайн-проект квартиры',
+        "Дизайн-проект квартиры",
         'Дизайн-проект квартиры ЖК "Сармановский',
         'Дизайн-проект квартиры ЖК "Притяжение"',
-        'Реконструкция частного дома Габишего'
+        "Реконструкция частного дома Габишего",
     ];
 
     const designProjects = [
@@ -19,38 +25,43 @@
         "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FAtjr%2FG%2Bo8td2Ga59oKs9eicQfwDla8PZBvGQYb9ClcBYG%2B%2F3MjCbb19yuEsWGF4xq%2FJ6bpmRyOJonT3VoXnDag%3D%3D&name=%D0%96%D0%9A%20%D0%A1%D0%B0%D1%80%D0%BC%D0%B0%D0%BD%D0%BE%D0%B2%D1%81%D0%BA%D0%B8%D0%B9.pdf&nosw=1",
         "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2F12p5WH%2Bx2sELBWTFCA63hPVAn9rSE%2FguLrN%2FxhntGJjPaQfTueGU1EoTI%2B%2BU9zJEq%2FJ6bpmRyOJonT3VoXnDag%3D%3D&name=%D0%96%D0%9A%20%D0%9F%D1%80%D0%B8%D1%82%D1%8F%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5%20%D0%B0%D0%BB%D1%8C%D0%B1%D0%BE%D0%BC_.pdf&nosw=1",
         "https://docs.yandex.ru/docs/view?url=ya-disk-public%3A%2F%2FBExuSusfEwRHV0v1PMpO0z%2Fo6qoYQ9oenzsJr2PT5PVR52tooOn2z3MHVQDYt9IDq%2FJ6bpmRyOJonT3VoXnDag%3D%3D&name=%D0%B0%D0%BB%D1%8C%D0%B1%D0%BE%D0%BC%20%D0%B0.%D0%BA%D1%83%D1%82%D1%83%D1%8F.pdf&nosw=1",
-    ]
+    ];
 </script>
 
 <section class="design">
     <section class="design__info">
-         <h1 id="heading5" class="section__title animated animate__animated animate__fadeIn">Дизайны</h1>
+        <h1
+            id="heading5"
+            class="section__title animated animate__animated animate__fadeIn"
+        >
+            Дизайны
+        </h1>
     </section>
 
     <section class="design__item_container">
-        <DesignItem 
+        <DesignItem
+            designItemBg={designBgs[0]}
             designItemText={designTexts[0]}
             designItemProject={designProjects[0]}
-        >
-        </DesignItem>
+        ></DesignItem>
 
-        <DesignItem 
+        <DesignItem
+            designItemBg={designBgs[1]}
             designItemText={designTexts[1]}
             designItemProject={designProjects[1]}
-        >
-        </DesignItem>
+        ></DesignItem>
 
-        <DesignItem 
+        <DesignItem
+            designItemBg={designBgs[2]}
             designItemText={designTexts[2]}
             designItemProject={designProjects[2]}
-        >
-        </DesignItem>
+        ></DesignItem>
 
-        <DesignItem 
+        <DesignItem
+            designItemBg={designBgs[3]}
             designItemText={designTexts[3]}
             designItemProject={designProjects[3]}
-        >
-        </DesignItem>
+        ></DesignItem>
     </section>
 </section>
 
@@ -68,13 +79,13 @@
 
         display: flex;
         flex-direction: column;
-}
+    }
 
     .design__item_container {
         width: 100%;
         height: auto;
 
-        display: grid; 
+        display: grid;
         grid-template-columns: repeat(2, 1fr);
         gap: 1vw;
 
--- src/components/DesignItem.svelte
+++ src/components/DesignItem.svelte
@@ -1,43 +1,76 @@
 <script>
+    export let designItemBg = "";
     export let designItemText = "";
     export let designItemProject = "";
 </script>
 
-<a href="{designItemProject}" target="_blank" class="design__item">
-    <p class="design__item_header">{designItemText}</p>
+<a
+    href={designItemProject}
+    target="_blank"
+    class="design__item"
+    style="
+        background: url('{designItemBg}');
+        background-size: cover;
+        background-position: center center;
+        --design-item-text: '{designItemText}';
+    "
+>
 </a>
 
 <style>
     .design__item {
+        position: relative;
         width: 100%;
-        min-height: 50vh;
+        min-height: 30vw;
         max-height: auto;
-
-
         display: flex;
         justify-content: center;
         align-items: center;
-
         font-size: 1.25vw;
         text-transform: uppercase;
         text-align: center;
         line-height: 2vw;
-
         border-radius: 20px;
         box-shadow: 0px 0px 5px 0px var(--accent-color);
-
         background-color: var(--secondary-color);
+        transition: 0.2s;
+    }
 
-        transition: .2s;
+    .design__item::before {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        border-radius: 20px;
+        backdrop-filter: blur(5px);
+        -webkit-backdrop-filter: blur(5px);
+        background: var(--secondary-color);
+        opacity: 0.9;
+        transition: opacity 0.3s ease;
+        z-index: 1;
+        pointer-events: none;
+    }
+
+    .design__item::after {
+        content: var(--design-item-text);
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        opacity: 1;
+        transition: opacity 0.3s ease;
+        z-index: 2;
+        pointer-events: none;
+        width: auto;
+        height: auto;
+        color: var(--text-color);
+        white-space: nowrap;
     }
 
     .design__item:hover {
-        transform: scale(1.02);
-        box-shadow: 0 0 10px 5px var(--accent-color);
-    }
-
-    .design__item_header {
-        color: var(--text-color);
+        box-shadow: 0px 0px 10px 5px var(--accent-color);
     }
 
     @media only screen and (max-width: 600px) {
